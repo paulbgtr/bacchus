57a56dfbed2d21c6631adae212e825e06ae588e9
paulbg <paulbg@hey.com>
2024-04-30 10:55:54 +0300

    feat: implement routing for bids

--- backend/router.js
+++ backend/router.js
@@ -1,9 +1,26 @@
 import express from "express";
+import {
+  getAllBids,
+  getBidById,
+  createNewBid,
+} from "./controllers/bids.controller.js";
 
 const router = express.Router();
 
+/**
+ * Default route
+ *
+ * Could be improved by adding a more descriptive message
+ */
 router.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+/**
+ * Bids routes
+ */
+router.get("/bids", getAllBids);
+router.get("/bids/:id", getBidById);
+router.post("/bids", createNewBid);
 
 export default router;
