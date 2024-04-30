160f3fe1b3d9508481ae0989c848e47fa0468eba
paulbg <paulbg@hey.com>
2024-04-30 10:54:27 +0300

    feat: add db module

---
+++ backend/db/db.js
@@ -0,0 +1,50 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import pkg from "pg";
+const { Client } = pkg;
+import { bids } from "./schema.js";
+
+/**
+ * A database client
+ * For the sake of simplicity, we are using the default values for the connection
+ *
+ */
+const client = new Client({
+  host: "127.0.0.1",
+  port: 5432,
+  user: "postgres",
+  password: "postgres",
+  database: "postgres",
+});
+
+await client.connect();
+const db = drizzle(client);
+
+/**
+ * Get all bids from the database
+ *
+ * @returns {Promise<QueryResult>}
+ */
+export const getBids = async () => {
+  return db.select(bids).execute();
+};
+
+/**
+ * Get a bid by id
+ *
+ * @param {number} id
+ * @returns {Promise<QueryResult>}
+ */
+export const getBid = async (id) => {
+  return db.select(bids).where(bids.id.eq(id)).execute();
+};
+
+/**
+ * Create a new bid
+ *
+ * @param {string} fullName
+ * @param {number} amount
+ * @returns {Promise<QueryResult>}
+ */
+export const createBid = async (fullName, amount) => {
+  return db.insert(bids).values({ fullName, amount }).execute();
+};
