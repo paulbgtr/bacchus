f1f18c126bc0341839d5b05c469d0fb1ea0df665
paulbg <paulbg@hey.com>
2024-04-30 10:55:03 +0300

    feat: implement simple bids controller

---
+++ backend/controllers/bids.controller.js
@@ -0,0 +1,54 @@
+import { getBids } from "../db/db.js";
+
+/**
+ * Get all bids by utilizing the getBids function from db.js
+ *
+ * @param {Request} req
+ * @param {Response} res
+ *
+ * @returns {Promise<void>}
+ */
+export const getAllBids = async (req, res) => {
+  try {
+    const bids = await getBids();
+    res.status(200).json(bids);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+/**
+ * Get a bid by id by utilizing the getBid function from db.js
+ *
+ * @param {Request} req
+ * @param {Response} res
+ *
+ * @returns {Promise<void>}
+ */
+export const getBidById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const bid = await getBid(id);
+    res.status(200).json(bid);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+/**
+ * Create a new bid by utilizing the createBid function from db.js
+ *
+ * @param {Request} req
+ * @param {Response} res
+ *
+ * @returns {Promise<void>}
+ */
+export const createNewBid = async (req, res) => {
+  try {
+    const { fullName, amount } = req.body;
+    await createBid(fullName, amount);
+    res.status(201).json({ message: "Bid created successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
