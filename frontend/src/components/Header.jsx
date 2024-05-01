54235b3c1766ccd9edbf962c890b7026cda611b3
paulbg <paulbg@hey.com>
2024-05-01 08:31:28 +0300

    feat: add Header component for root page

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+  return (
+    <section className="min-h-[75vh] grid justify-center place-content-center">
+      <header className=" space-y-2">
+        <h1 className="font-bold text-4xl">Bacchus</h1>
+        <p>An auction app when you can place bids on different stuff</p>
+        <p>Have fun</p>
+      </header>
+    </section>
+  );
+};
